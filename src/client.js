5c4a1f3c712cc8e514ce384a06e5ee966081abb4
Jean Pierre Huaman <jeanpipoxi@gmail.com>
2022-04-17 17:15:43 -0500

    fix env variables

--- src/client.js
+++ src/client.js
@@ -2,11 +2,11 @@ import sanityClient from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
 export const client = sanityClient({
-  projectId: import.meta.env.VITE_SOME_KEY,
+  projectId: import.meta.env.VITE_API_KEY,
   dataset: 'production',
   apiVersion: '2022-03-05',
   useCdn: true,
-  token: process.env.REACT_APP_TOKEN,
+  token: import.meta.env.REACT_APP_TOKEN,
 });
 
 const builder = imageUrlBuilder(client);
